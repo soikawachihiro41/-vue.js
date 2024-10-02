ff909f20a7e0c7c581a8b04bb089e5700d5bfe05
soikawachihiro41 <soikawa0108@gmail.com>
2024-10-02 16:54:34 +0900

    5章

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import MainTodo from '@/components/MainTodo.vue';
 import TheFooter from '@/components/TheFooter.vue';
 import TheHeader from '@/components/TheHeader.vue';
 
@@ -8,7 +9,7 @@ import TheHeader from '@/components/TheHeader.vue';
 <template>
   <div class="wrap">
     <TheHeader />
-    <main class="main">メイン</main>
+    <main class="main"><MainTodo /></main>
     <TheFooter />
   </div>
 </template>
---
+++ src/components/MainTodo.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const todo = ref('');
+const todoList = ref<{ id: number; task: string }[]>([]);
+
+const addTodo = () => {
+
+  const id = new Date().getTime();
+
+
+  todoList.value.push({ id: id, task: todo.value });
+
+  localStorage.todoList = JSON.stringify(todoList.value);
+
+  todo.value = '';
+};
+</script>
+
+<template>
+  <div class="box_input">
+  <input type="text"
+   class="todo_input" 
+   v-model="todo"
+   placeholder="+ TODOを入力"
+    />
+  <button class="btn" @click="addTodo">追加</button> </div>
+</template>
+
+<style scoped>
+.todo_input {
+  width: 250px;
+  padding: 6px 8px;
+  margin-right: 8px;
+  font-size: 18px;
+  border: 1px solid #aaa;
+  border-radius: 6px;
+}
+.btn {
+  position: relative;
+  padding: 6px 8px;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+  background-color: #03a9f4;
+  border: 1px solid #eee;
+  border-radius: 6px;
+}
+.btn:active {
+  top: 1px;
+} </style>
