12183ab91d479ab868d6da91141e77230a823f81
soikawachihiro41 <soikawa0108@gmail.com>
2024-10-02 18:12:53 +0900

    6章

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const todo = ref('');
-const todoList = ref<{ id: number; task: string }[]>([]);
+const todoList = ref<{ id: number; task: string }[]>([
+]);
+
+const ls = localStorage.todoList;
+
+todoList.value = ls ? JSON.parse(ls) : [];
 
 const addTodo = () => {
 
@@ -14,16 +19,36 @@ const addTodo = () => {
 
   todo.value = '';
 };
+
+const todoExample = ref(['example','example','example3', 'example4"']);
 </script>
 
 <template>
   <div class="box_input">
-  <input type="text"
-   class="todo_input" 
-   v-model="todo"
-   placeholder="+ TODOを入力"
+    <input type="text"
+     class="todo_input" 
+     v-model="todo"
+     placeholder="+ TODOを入力"
     />
-  <button class="btn" @click="addTodo">追加</button> </div>
+    <button class="btn" @click="addTodo">追加</button> 
+  </div>
+  <div class="box_list">
+    <div class="todo_list" v-for="todo in todoList" :key="todo.id">
+      <div class="todo">
+        <input type="checkbox" class="check" />
+        <label>{{ todo.task }}</label>
+      </div>
+      <div class="btns">
+        <button class="btn green">編</button>
+        <button class="btn pink">削</button>
+      </div>
+    </div>
+  </div>
+  <div v-for="(example, index) in todoExample" :key="index">
+    <p>{{ index }}.{{ example }}</p>
+  </div>
+
+
 </template>
 
 <style scoped>
@@ -47,4 +72,36 @@ const addTodo = () => {
 }
 .btn:active {
   top: 1px;
-} </style>
+}
+.box_list {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  margin-top: 20px;
+}
+.todo_list {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+.todo {
+  width: 250px;
+  padding: 6px 8px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+}
+.check {
+  margin-right: 12px;
+  transform: scale(1.6);
+}
+.btns {
+  display: flex;
+  gap: 4px;
+}
+.green {
+  background-color: #00c853;
+}
+.pink {
+  background-color: #ff4081;
+}
+</style>
